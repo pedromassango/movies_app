f3958ff89ffd631d4c7b9c013b36e79dd7730a01
Pedro Massango <pedromassango.developer@gmail.com>
2021-04-02 00:22:27 +0100

    Fix content scroll in movie details page

--- lib/src/presentation/movie_details_page/movie_details_page.dart
+++ lib/src/presentation/movie_details_page/movie_details_page.dart
@@ -66,80 +66,86 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
           ),
           Align(
             alignment: Alignment(0, 1),
-            child: SingleChildScrollView(
-              physics: BouncingScrollPhysics(),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: SizedBox(
+                height: imageHeight * .8,
+                child: ListView(
+                  padding: EdgeInsets.only(bottom: 32),
+                  physics: BouncingScrollPhysics(),
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 54),
-                      child: Text(widget.movie.title, style: textTheme.headline5!.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900
-                      ),),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8, bottom: 24),
-                      child: Text(widget.movie.getFormattedReleaseDate(),
-                        style: textTheme.headline6!.copyWith(
-                            color: Colors.white60,
-                            fontWeight: FontWeight.w800,
-                      ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          BlocBuilder<MovieDetailsCubit, MovieDetailsState>(
-                            builder: (context, state) {
-                              final status = state.hasData ? state.movieDetails!.status : 'Loading...';
-                              return Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 4),
-                                child: _Section(
-                                  header: 'Status',
-                                  horizontalAlignment: CrossAxisAlignment.center,
-                                  child: Text(status),
-                                ),
-                              );
-                            },
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 54),
+                          child: Text(widget.movie.title, style: textTheme.headline5!.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900
+                          ),),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8, bottom: 24),
+                          child: Text(widget.movie.getFormattedReleaseDate(),
+                            style: textTheme.headline6!.copyWith(
+                                color: Colors.white60,
+                                fontWeight: FontWeight.w800,
                           ),
-                          BlocBuilder<MovieDetailsCubit, MovieDetailsState>(
-                            builder: (context, state) {
-                              final voteAverage = state.hasData ? state.movieDetails!.movie.rating : 0.0;
-                              return Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 4),
-                                child: _Section(
-                                  header: 'Popularity',
-                                  horizontalAlignment: CrossAxisAlignment.center,
-                                  child: Text('$voteAverage'),
-                                ),
-                              );
-                            },
                           ),
-                          if (widget.movie.hasGenres())
-                            _Section(
-                              header: 'Genres',
-                              horizontalAlignment: CrossAxisAlignment.center,
-                              child: Text(widget.movie.getGenresAsString()),
-                            ),
-                        ],
-                      ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              BlocBuilder<MovieDetailsCubit, MovieDetailsState>(
+                                builder: (context, state) {
+                                  final status = state.hasData ? state.movieDetails!.status : 'Loading...';
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                                    child: _Section(
+                                      header: 'Status',
+                                      horizontalAlignment: CrossAxisAlignment.center,
+                                      child: Text(status),
+                                    ),
+                                  );
+                                },
+                              ),
+                              BlocBuilder<MovieDetailsCubit, MovieDetailsState>(
+                                builder: (context, state) {
+                                  final voteAverage = state.hasData ? state.movieDetails!.movie.rating : 0.0;
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                                    child: _Section(
+                                      header: 'Popularity',
+                                      horizontalAlignment: CrossAxisAlignment.center,
+                                      child: Text('$voteAverage'),
+                                    ),
+                                  );
+                                },
+                              ),
+                              if (widget.movie.hasGenres())
+                                _Section(
+                                  header: 'Genres',
+                                  horizontalAlignment: CrossAxisAlignment.center,
+                                  child: Text(widget.movie.getGenresAsString()),
+                                ),
+                            ],
+                          ),
+                        ),
+                        _Section(
+                          header: 'Overview',
+                          child: Text(widget.movie.overview),
+                        ),
+                        BlocBuilder<MovieDetailsCubit, MovieDetailsState>(
+                          builder: (context, state) {
+                            if (!state.hasData || !state.hasCompanies)
+                              return SizedBox.shrink();
+                            return _CompaniesSection(state.movieDetails!.companies);
+                          },
+                        )
+                      ],
                     ),
-                    _Section(
-                      header: 'Overview',
-                      child: Text(widget.movie.overview),
-                    ),
-                    BlocBuilder<MovieDetailsCubit, MovieDetailsState>(
-                      builder: (context, state) {
-                        if (!state.hasData || !state.hasCompanies)
-                          return SizedBox.shrink();
-                        return _CompaniesSection(state.movieDetails!.companies);
-                      },
-                    )
                   ],
                 ),
               ),
